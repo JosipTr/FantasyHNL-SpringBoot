a1cac63abbe7c2e3d698e1a2467733cbb438c0dd
JosipTr <jtr.other13@gmail.com>
2024-02-05 19:20:07 +0100

    Initial commit

---
+++ src/main/java/com/example/fantasyhnl/FantasyhnlApplication.java
@@ -0,0 +1,13 @@
+package com.example.fantasyhnl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyhnlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyhnlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fantasyhnl/FantasyhnlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fantasyhnl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyhnlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
